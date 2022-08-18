3f0ab95c75b62f0f409c8519e7b7f0adfa4c5ffa
Mark Kostevych <markkostevych@gmail.com>
2022-08-18 20:59:46 +0300

    parallax

--- src/components/AppHeading.vue
+++ src/components/AppHeading.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
     <h1
-      class="relative text-[82px] font-bold leading-[110%] uppercase text-primary text-center after:absolute after:w-[578px] after:h-[578px] after:rounded-full after:bg-yellow after:blur-[35px] after:top-1/2 after:left-1/2 after:transform after:-translate-y-1/2 after:-translate-x-1/2 after:z-[-1]"
+      ref="heading"
+      class="relative text-[82px] font-bold leading-[110%] uppercase text-primary text-center"
     >
       {{ title }}
     </h1>
+
+    <!-- background circle -->
+    <div
+      class="absolute w-[578px] h-[578px] rounded-full bg-yellow blur-[35px] top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 z-[-1]"
+    />
+
+    <!-- spin icon -->
     <div
       class="w-[578px] h-[578px] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
     >
@@ -25,6 +33,27 @@ export default {
     showSpinner: {
       type: Boolean,
     },
+  },
+  data() {
+    return {
+      animSpeed: 1,
+    };
+  },
+  methods: {
+    parallax(e) {
+      if (!this.$refs.heading) return;
+
+      const x = (window.innerWidth - e.pageX * this.animSpeed) / 100;
+      const y = (window.innerHeight - e.pageY * this.animSpeed) / 100;
+
+      this.$refs.heading.style.transform = `translateX(-${x}px) translateY(-${y}px)`;
+    },
+  },
+  mounted() {
+    document.addEventListener("mousemove", this.parallax);
+  },
+  beforeUnmount() {
+    document.removeEventListener("mousemove", this.parallax);
   },
 };
 </script>
